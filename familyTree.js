14eceb02258110a10daf29ee30a41554f8ed04d4
Mahdi2105 <mahdih2105@gmail.com>
2021-10-13 16:29:56 +0100

    familyTree

--- familyTree.js
+++ familyTree.js
@@ -98,5 +98,6 @@ var gen3 = [
     },
   ];
   //Type Gen 1,2 or 3 and then the index to get the parents
-  console.log(gen2[3].childOf())
+  console.log(gen1[0].childOf())
   
+  module.exports = {gen1, gen2, gen3};
--- familyTree.test.js
+++ familyTree.test.js
@@ -1,7 +1,14 @@
-const {gen1, gen2, gen3} = require ("familyTree.js") ;
+
+const {gen1, gen2, gen3} = require ('./familyTree');
+
+describe("Family Tree", () => {
+    test("Should return name", () => {
+        expect(gen1[0].name).toBe("Elizabeth II")
+    })
+});
 
 describe("Family Tree", () => {
     test("Should return parents", () => {
-        expect(gen1[0].name).toBe("Elizabeth II")
+        expect(gen1[0].childOf()).toStrictEqual(["George", "Elizabeth"])
     })
 });
