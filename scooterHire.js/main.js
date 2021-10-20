ebb39d59361e796c1aecde56ac54895daf870526
Mahdi2105 <mahdih2105@gmail.com>
2021-10-20 15:06:39 +0100

    update

---
+++ scooterHire.js/main.js
@@ -0,0 +1,23 @@
+const User = require ("./user")
+const Scooter = require ("./scooter")
+const ScooterHire = require ("./scooterHire")
+
+user1 = new User ("James", 20, 150.00)
+user2 = new User ("Mohammed", 21, 130.00)
+user3 = new User ("Emily", 19, 140.00)
+user4 = new User ("Philip", 22, 140.00)
+user5 = new User ("Hannah", 21, 150.00)
+
+scooter1 = new Scooter ("Scooter01", "Canary Wharf", "Fixed", 100)
+scooter2 = new Scooter ("Scooter02", "Canary Wharf", "Fixed", 70)
+scooter3 = new Scooter ("Scooter03", "Mile End", "Broken", 100)
+scooter4 = new Scooter ("Scooter04", "Mile End", "Fixed", 100)
+scooter5 = new Scooter ("Scooter05", "Whitechapel", "Broken", 40)
+
+hire1 = new ScooterHire()
+hire1.hireScooter(scooter1, user1)
+//console.log(hire1)
+//hire1.return(scooter1, "Mile End")
+hire1.reportBroken(scooter1, "Mile End")
+//console.log(scooter1)
+//console.log(user1)
--- scooterHire.js/scooterHire.js
+++ scooterHire.js/scooterHire.js
@@ -1,18 +1,3 @@
-const User = require ("./user")
-const Scooter = require ("./scooter")
-
-user1 = new User ("James", 20, 150.00)
-user2 = new User ("Mohammed", 21, 130.00)
-user3 = new User ("Emily", 19, 140.00)
-user4 = new User ("Philip", 22, 140.00)
-user5 = new User ("Hannah", 21, 150.00)
-
-scooter1 = new Scooter ("Scooter01", "Canary Wharf", "Fixed", 100)
-scooter2 = new Scooter ("Scooter02", "Canary Wharf", "Fixed", 70)
-scooter3 = new Scooter ("Scooter03", "Mile End", "Broken", 100)
-scooter4 = new Scooter ("Scooter04", "Mile End", "Fixed", 100)
-scooter5 = new Scooter ("Scooter05", "Whitechapel", "Broken", 40)
-
 class ScooterHire {
     constructor(scooterID, hiredBy) {
         this.scooterID = []
@@ -57,10 +42,10 @@ class ScooterHire {
         this.charge(scooter)
     }
     
-    async reportBroken(scooter) {
+    async reportBroken(scooter, station) {
         scooter.condition = "Broken"
         console.log("Thank you for reporting")
-        this.return(scooter, scooter.station)
+        this.return(scooter, station)
     }
 
     async return(scooter, station) {
@@ -72,13 +57,5 @@ class ScooterHire {
         }
     }
 };
-console.log(scooter1)
-hire1 = new ScooterHire()
-hire1.hireScooter(scooter1, user1)
-//console.log(hire1)
-//hire1.return(scooter1, "Mile End")
-hire1.reportBroken(scooter1)
-console.log(scooter1)
-//console.log(user1)
 
 module.exports = ScooterHire;
