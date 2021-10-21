b58c35c109c1656693ca389becac67c84e6a391b
Mahdi2105 <mahdih2105@gmail.com>
2021-10-21 10:15:47 +0100

    async test

--- scooterHire/tests/scooterApp.test.js
+++ scooterHire/tests/scooterApp.test.js
@@ -2,9 +2,13 @@ const User = require ("../src/user")
 const Scooter = require ("../src/scooter")
 const ScooterHire = require ("../src/scooterHire")
 
+let user1 = new User ("James", 20, 150.00)
+let scooter1 = new Scooter ("Scooter01", "Canary Wharf", "Fixed", 100)
+let hire1 = new ScooterHire (scooter1, user1)
+
 // User Tests
 describe("User Tests", () => {
-    let user1 = new User ("James", 20, 150.00)
+    
     test("Check user's name is a string", () => {
         expect(typeof User.users[0].name).toBe("string")
     })
@@ -15,7 +19,7 @@ describe("User Tests", () => {
 
 // Scooter Tests
 describe("Scooter Tests", () => {
-    scooter1 = new Scooter ("Scooter01", "Canary Wharf", "Fixed", 100)
+    
     test("Check scooter station", () => {
         expect(Scooter.scooters[0].station).toBe("Canary Wharf")
     })
@@ -23,3 +27,9 @@ describe("Scooter Tests", () => {
         expect(Scooter.scooters[0].available).toBe(true)
     })
 });
+
+//Async tests 
+test("charge", async () => {
+    await hire1.charge(scooter1); // we need to wait for the charge!
+    console.log("Test complete");
+  });
