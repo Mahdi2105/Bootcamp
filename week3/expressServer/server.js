09e1a6457ecc86ec603dbb477c7b949977669d03
Mahdi2105 <mahdih2105@gmail.com>
2021-10-28 16:19:35 +0100

    fixed index and style for server

--- week3/expressServer/server.js
+++ week3/expressServer/server.js
@@ -1,37 +1,40 @@
 const express = require('express');
 
+
 // network address to accept connections on
 const port = 3001;
 
 // assign instance of express to app variable
 const app = express();
 
+app.use(express.static('public'));
+
 // handle http://localhost:3001
 app.get('/', (request, response) => {
   response.send(`This is a simple static response`);
 });
 
+// // handle http://localhost:3001/restaurant/1 - dynamic route parameters
+// app.get('/restaurant/:id', (request, response) => {
+//   // :id = a dynamic (changeable) parameter
+
+//   response.send(
+//     `Hello, you asked for the restaurant with an ID of ${request.params.id}`
+//   );
+// });
+
 // handle http://localhost:3001/restaurant/1 - dynamic route parameters
 app.get('/restaurant/:id', (request, response) => {
-  // :id = a dynamic (changeable) parameter
-
-  response.send(
-    `Hello, you asked for the restaurant with an ID of ${request.params.id}`
-  );
-});
-
-// handle http://localhost:3001/crypto/1 - dynamic route parameters
-app.get('/crypto/:id', (request, response) => {
-  // mock fetch crypto id from database
-  obj = null;
-
+  // mock fetch restaurant id from database
+  let obj = null;
   if (request.params.id === '1') {
-    obj = {
-      id: request.params.id,
-      name: 'Bitcoin',
-    };
-  } else {
-    obj = 'Some other coin';
+    obj = "Pizza Hut";
+  } 
+  else if (request.params.id === '2') {
+    obj = "Drinks4Us";
+  }
+  else {
+    obj = 'Restaurant not found';
   }
 
   response.send(obj);
@@ -41,3 +44,9 @@ app.get('/crypto/:id', (request, response) => {
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
 });
+
+// app.get('/index.html', (request, response) => {
+//   // mock fetch restaurant id from database
+//   response.render("index");
+// });
+
